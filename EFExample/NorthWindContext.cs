df1edce4b921f0598b5648132e053bfbf1c81715
Henrik Bulskov <bulskov@ruc.dk>
2020-10-22 11:03:35 +0200

    Making EF context

---
+++ EFExample/NorthWindContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFExample
+{
+    public class NorthWindContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+            optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+            modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+        }
+    }
+
+    
+}
---
+++ EFExample/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+
+namespace EFExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var ctx = new NorthWindContext();
+
+            //var maxId = ctx.Categories.Max(x => x.Id);
+
+            //ctx.Categories.Add(new Category {Id = maxId + 1, Name = "Testing"});
+
+            ctx.SaveChanges();
+
+            foreach (var category in ctx.Categories)
+            {
+                Console.WriteLine(category);
+            }
+        }
+    }
+}
