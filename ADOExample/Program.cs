01d34d8a1600277f08ba52b5f727628053a4d6d3
Henrik Bulskov <bulskov@ruc.dk>
2020-10-22 09:46:43 +0200

    Add project files.

---
+++ ADOExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Npgsql;
+
+namespace ADOExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = "select categoryid, categoryname from categories";
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"Id={reader.GetInt32(0)}, Name={reader.GetString(1)}");
+            }
+        }
+    }
+}
